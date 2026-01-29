19dfeef91528b1ac087e3be79976ded2c6b9287e
rafael-bispo <rafaeldossantosbispo@gmail.com>
2026-01-29 09:25:49 -0300

    Subindo o estudo pro github

---
+++ src/main/java/br/com/estudo/imageliteapi/application/images/ImagesController.java
@@ -0,0 +1,27 @@
+package br.com.estudo.imageliteapi.application.images;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/images")
+@Slf4j
+public class ImagesController {
+
+    @PostMapping
+    public ResponseEntity save(
+            @RequestParam("file") MultipartFile file,
+            @RequestParam("name") String name,
+            @RequestParam("tags") List<String> tags
+            ){
+        log.info("Dados da imagem recebida: \nName: {}\nNome original da imagem: {}\nSize: {}\nTags: {}", name, file.getOriginalFilename(), file.getSize(), tags);
+        return ResponseEntity.ok().build();
+    }
+}
